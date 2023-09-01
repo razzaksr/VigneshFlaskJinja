f4ef1d9cec94ef7c3113d63af39e6d46f9936c65
razzaksr <razzaksr@gmail.com>
2023-09-01 12:08:50 +0530

    UI List, Create with Flask and MongoDB

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template, request, redirect
 from db import *
 
 app=Flask(__name__)
@@ -7,7 +7,34 @@ app=Flask(__name__)
 app.config['MONGODB_HOST']=url
 mydb.init_app(app)
 
+@app.route("/new",methods=['GET','POST'])
+def newOne():
+    if request.method=="GET":
+        return render_template("newlaptop.html")
+    else:
+        laptop=Laptop()
+        laptop.model=request.form['model']
+        laptop.serial=request.form['serial']
+        laptop.ram=int(request.form['ram'])
+        laptop.ssd=int(request.form['ssd'])
+        laptop.price=int(request.form['price'])
+        laptop.stock=int(request.form['stock'])
+        laptop.type=request.form['type']
+        
+        laptop.save()
+        
+        return redirect("/list")
+
 @app.route("/")
+def showHome():
+    return render_template("navigation.html")
+
+@app.route("/list")
+def listAll():
+    collected=Laptop.objects.all()
+    return render_template("view.html",data=collected)
+
+@app.route("/test")
 def checkConnection():
     #return make_response("<h1>Hell</h1>")
     return jsonify(Laptop.objects.all())
